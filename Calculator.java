d2c36fcd0ba187a853c649e594949929024f885e
harriswarrich <harris.warrich786@gmail.com>
2024-10-03 00:09:46 -0500

    Add division functionality

--- Calculator.java
+++ Calculator.java
@@ -11,11 +11,19 @@ public class Calculator{
     public int multiply(int a, int b) {
         return a * b;
     }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }    
     
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("Add: " + calc.add(3,4));
         System.out.println("Subtract: " + calc.subtract(3,4));
         System.out.println("Multiply: " + calc.multiply(3,4));
+        System.out.println("Divide: " + calc.divide(3,4));
     }
 }
