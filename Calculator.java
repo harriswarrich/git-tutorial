29a8fbc6f7631f30e9219fbf6351c605eccb09f4
harriswarrich <harris.warrich786@gmail.com>
2024-10-02 22:06:19 -0500

    Add subtraction functionality

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,13 @@ public class Calculator{
         return a + b;
     }
 
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("Add: " + calc.add(3,4));
+        System.out.println("Subtract: " + calc.subtract(3,4));
     }
 }
